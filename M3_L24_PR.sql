8669a32bd61b7bc7e0515df9070b411374b90fee
Bartosz Bak <b.bak208@gmail.com>
2020-10-14 09:22:04 +0200

    Add DROP TABLE IF EXISCS

--- M3_L24_PR.sql
+++ M3_L24_PR.sql
@@ -1,31 +1,35 @@
---- 1.  Create schema expense_tracker.
-CREATE SCHEMA expense_tracker;
 
---- 2. Create table bank_account_owner.
+--- 1.  Create nad drop schema expense_tracker.
+DROP SCHEMA IF EXISTS expense_tracker CASCADE;
+CREATE SCHEMA IF NOT EXISTS expense_tracker;
+
+--- 2. Create nad drop table bank_account_owner.
+DROP TABLE IF EXISTS expense_tracker.bank_account_owner CASCADE;
 CREATE TABLE IF NOT EXISTS expense_tracker.bank_account_owner(
 	id_ba_own integer PRIMARY KEY,
-	owner_name varchar(100) NOT NULL,
+	owner_name varchar(50) NOT NULL,
 	owner_desc varchar(250), 
 	user_login integer NOT NULL, 
-	active boolean DEFAULT '1' NOT NULL,
+	active boolean DEFAULT TRUE NOT NULL,
 	insert_date timestamp DEFAULT current_timestamp,
 	update_date timestamp DEFAULT current_timestamp
 );
 
---- 3. Create table bank_account_types.
+--- 3. Create nad drop table bank_account_types.
+DROP TABLE IF EXISTS expense_tracker.bank_account_types CASCADE;
 CREATE TABLE IF NOT EXISTS expense_tracker.bank_account_types(
 	id_ba_type integer PRIMARY KEY,
 	ba_type varchar(50) NOT NULL,
 	ba_desc varchar(250),
-	active boolean DEFAULT '1' NOT NULL ,
-	is_common_account boolean DEFAULT '1' NOT NULL,
+	active boolean DEFAULT TRUE NOT NULL ,
+	is_common_account boolean DEFAULT TRUE NOT NULL,
 	id_ba_own integer,
 	insert_date timestamp DEFAULT current_timestamp,
 	update_date timestamp DEFAULT current_timestamp
 );
 
---- 4. Create table transactions.
-
+--- 4. Create nad drop table transactions.
+DROP TABLE IF EXISTS expense_tracker.transactions CASCADE;
 CREATE TABLE IF NOT EXISTS expense_tracker.transactions(
 	id_transaction integer PRIMARY KEY,
 	id_trans_ba integer,
@@ -40,62 +44,62 @@ CREATE TABLE IF NOT EXISTS expense_tracker.transactions(
 	update_date timestamp DEFAULT current_timestamp 
 );
 
---- 5. Create table transaction_bank_account.
-	
+--- 5. Create nad drop table transaction_bank_account.
+DROP TABLE IF EXISTS expense_tracker.transaction_bank_accounts CASCADE;	
 CREATE TABLE IF NOT EXISTS expense_tracker.transaction_bank_accounts(
 	id_trans_ba integer PRIMARY KEY,
 	id_ba_own integer,
 	id_ba_typ integer,
 	bank_account_name varchar(50) NOT NULL,
 	bank_account_desc varchar(250),
-	active boolean DEFAULT '1' NOT NULL,
+	active boolean DEFAULT TRUE NOT NULL,
 	insert_date timestamp DEFAULT current_timestamp,
 	update_date timestamp DEFAULT current_timestamp
 	);
 
---- 6. Create table transaction_category
-
+--- 6. Create nad drop table transaction_category
+DROP TABLE IF EXISTS expense_tracker.transaction_category CASCADE;
 CREATE TABLE IF NOT EXISTS expense_tracker.transaction_category(
 	id_trans_cat integer PRIMARY KEY,
 	category_name varchar(50) NOT NULL,
 	category_description varchar(250),
-	active boolean DEFAULT '1' NOT NULL,
+	active boolean DEFAULT TRUE NOT NULL,
 	insert_date timestamp DEFAULT current_timestamp,
 	update_date timestamp DEFAULT current_timestamp
 );
 
---- 7. Create table transaction_subcategory.
-
+--- 7. Create nad drop table transaction_subcategory.
+DROP TABLE IF EXISTS expense_tracker.transaction_subcategory CASCADE;
 CREATE TABLE IF NOT EXISTS expense_tracker.transaction_subcategory(
 	id_trans_subcat integer PRIMARY KEY,
 	id_trans_cat integer,
 	subcategory_name varchar(50) NOT NULL,
 	subcategory_description varchar(250),
-	active boolean DEFAULT '1' NOT NULL,
+	active boolean DEFAULT TRUE NOT NULL,
 	insert_date timestamp DEFAULT current_timestamp,
 	update_date timestamp DEFAULT current_timestamp
 );
 
---- 8. Create table transaction_type
-
+--- 8. Create nad drop table transaction_type
+DROP TABLE IF EXISTS expense_tracker.transaction_type CASCADE;
 CREATE TABLE IF NOT EXISTS expense_tracker.transaction_type(
 	id_trans_type integer PRIMARY KEY,
 	transaction_type_name varchar(50) NOT NULL,
 	transaction_type_desc varchar(250),
-	active boolean DEFAULT '1' NOT NULL,
+	active boolean DEFAULT TRUE NOT NULL,
 	insert_date timestamp DEFAULT current_timestamp,
 	update_date timestamp DEFAULT current_timestamp
 );
 
---- 9. Create table users.
-
+--- 9. Create nad drop table users.
+DROP TABLE IF EXISTS expense_tracker.users CASCADE;
 CREATE TABLE IF NOT EXISTS expense_tracker.users(
 	id_user integer PRIMARY KEY,
 	user_login varchar(25) NOT NULL,
 	user_name varchar(50) NOT NULL,
 	user_password varchar(100) NOT NULL,
 	password_salt varchar(100) NOT NULL,
-	active boolean DEFAULT '1' NOT NULL,
+	active boolean DEFAULT TRUE NOT NULL,
 	insert_date timestamp DEFAULT current_timestamp,
 	update_date timestamp DEFAULT current_timestamp
 );
