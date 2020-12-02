f5a209c920aaed90cd79c904bc6284e4c856f458
Bartosz Bak <b.bak208@gmail.com>
2020-12-02 10:41:31 +0100

    Add exercises from project M9

--- M9_L13_PR.sql
+++ M9_L13_PR.sql
@@ -61,9 +61,6 @@ CREATE OR REPLACE VIEW JiG_view AS
 	AND  bao.owner_name = 'Janusz i Gra¿ynka';	
 
 
-
-
-
 DROP VIEW JK_view;
 DROP VIEW GK_view;
 DROP VIEW JiG_view;
@@ -93,16 +90,16 @@ roku (do obu atrybutów BUDGET_PLANNED, LEFT_BUDGET ta sama wartoœæ) */
 		
 DROP TABLE IF EXISTS expense_tracker.monthly_budget_planned;		   
 CREATE TABLE IF NOT EXISTS expense_tracker.monthly_budget_planned	 (
-   year_month VARCHAR(7) PRIMARY KEY,
+   month_year VARCHAR(7) PRIMARY KEY,
    budget_planned NUMERIC(10,2),
    left_budget NUMERIC(10,2)
 );
 
-INSERT INTO expense_tracker.monthly_budget_planned(year_month, budget_planned, left_budget) VALUES ('12-2020', 1000, 1000);
+INSERT INTO expense_tracker.monthly_budget_planned(month_year, budget_planned, left_budget) VALUES ('12-2020', 1000, 1000);
 
 
 /*4. Dodaj nowy Wyzwalacz do tabeli TRANSACTIONS, który przy ka¿dorazowym dodaniu
-/ zaktualizowaniu lub usuniêciu wartoœci zmieni wartoœæ LEFT_BUDGET odpowiednio
+zaktualizowaniu lub usuniêciu wartoœci zmieni wartoœæ LEFT_BUDGET odpowiednio
 w tabeli expense_tracker.monthly_budget_planned. */
 
 CREATE OR REPLACE FUNCTION update_monthly_budget()
@@ -112,13 +109,13 @@ CREATE OR REPLACE FUNCTION update_monthly_budget()
 	 BEGIN 
 		IF (TG_OP = 'DELETE') THEN 
 		  UPDATE expense_tracker.monthly_budget_planned  SET left_budget = left_budget - OLD.transaction_value
-		  WHERE expense_tracker.monthly_budget_planned.year_month = (EXTRACT ( MONTH FROM OLD.transaction_date) ||'-'||  EXTRACT (YEAR FROM OLD.transaction_date));
+		  WHERE expense_tracker.monthly_budget_planned.month_year = (EXTRACT ( MONTH FROM OLD.transaction_date) ||'-'||  EXTRACT (YEAR FROM OLD.transaction_date));
 		ELSIF (TG_OP = 'UPDATE') THEN 
 		  UPDATE expense_tracker.monthly_budget_planned  SET left_budget = left_budget - OLD.transaction_value + NEW.transaction_value
-		  WHERE expense_tracker.monthly_budget_planned.year_month = (EXTRACT ( MONTH FROM  OLD.transaction_date) ||'-'||  EXTRACT (YEAR FROM OLD.transaction_date));
+		  WHERE expense_tracker.monthly_budget_planned.month_year = (EXTRACT ( MONTH FROM  OLD.transaction_date) ||'-'||  EXTRACT (YEAR FROM OLD.transaction_date));
 		ELSIF (TG_OP = 'INSERT') THEN 
 		  UPDATE expense_tracker.monthly_budget_planned  SET left_budget = left_budget + NEW.transaction_value
-		  WHERE expense_tracker.monthly_budget_planned.year_month = (EXTRACT ( MONTH FROM NEW.transaction_date) ||'-'||  EXTRACT (YEAR FROM NEW.transaction_date));
+		  WHERE expense_tracker.monthly_budget_planned.month_year = (EXTRACT ( MONTH FROM NEW.transaction_date) ||'-'||  EXTRACT (YEAR FROM NEW.transaction_date));
 		
 		END IF; 
 	RETURN NULL;
@@ -132,22 +129,25 @@ CREATE TRIGGER uptade_monthly_budget_trigger
 	EXECUTE PROCEDURE update_monthly_budget();
 
 /*5. Przetestuj dzia³anie wyzwalacza dla kilku przyk³adowych operacji. */
-ALTER SEQUENCE expense_tracker.transactions_id_transaction_seq
+
+/*ALTER SEQUENCE expense_tracker.transactions_id_transaction_seq
 	INCREMENT BY 1
 	MINVALUE 1
 	MAXVALUE 2147483647
 	START 7117
 	CACHE 1
 	NO CYCLE;
-ALTER SEQUENCE expense_tracker.transactions_id_transaction_seq RESTART;
+ALTER SEQUENCE expense_tracker.transactions_id_transaction_seq RESTART;*/
 
 
 INSERT INTO expense_tracker.transactions (id_trans_ba, id_trans_cat, id_trans_subcat, 
                           id_trans_type, id_user, transaction_date, transaction_value,transaction_description) 
                        VALUES (1,1,4,2,null,now(),-500,'f17');
                       
-UPDATE expense_tracker.transactions SET transaction_value = -200
+                      
+UPDATE expense_tracker.transactions SET transaction_value = -200 -- , transaction_date = '2020-11-02'
 									WHERE id_transaction = 7117;        
+														
 SELECT *
 FROM expense_tracker.monthly_budget_planned mbp;
 
@@ -159,5 +159,14 @@ FROM expense_tracker.monthly_budget_planned mbp;
 /*6. Czego brakuje w tym triggerze? Jakie potencjalnie spowoduje problemy w kontekœcie
 danych w tabeli MONTHLY_BUDGET_PLANNED. */
 
+--1) Mo¿na dodaæ funkcjê, która bêdzie aktualizowa³a planowany bud¿et na nastêpny miesi¹c. 
+--   Jeœli przekroczymy planowany bud¿et na dany miesi¹c to automatycznie zmniejszy iloœæ pieniêdzy jak¹ mo¿emy wydaæ w nastêpny miesi¹cu
+--   lub jeœli bêdziemy mieæ na koniec miesi¹ca wiêcej pieniêdzy ni¿ planowaliœmy automatycznie zwiêkszy iloœæ pieniêdzy jak¹ mo¿emy wydaæ w nastêpny miesi¹cu.
+--2) Trigger dzia³a niepoprawnie jeœli w operacji uptade zmienimy datê transakcji. 
+--3) Trigger nie jest odporny na ró¿ne formaty dat w kolumnie month_year.
+--4) Do tabeli MONTHLY_BUDGET_PLANNED mo¿na by by³o dodaæ informacjê o jakiego konta dotyczy planowany bud¿et i do³¹czyæ j¹ do schematu.
 
 
+
+DROP TABLE IF EXISTS expense_tracker.monthly_budget_planned;	
+DROP FUNCTION update_monthly_budget() CASCADE;
